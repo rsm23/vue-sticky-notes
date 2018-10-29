aee549c0e1bbcaab97b3dc3105915aa5328b9b77
Rahmani Seif El Moulouk <cool2309@gmail.com>
2018-10-29 20:29:33 +0100

    Initial commit and boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Header />
+        <img alt="Vue logo" src="./assets/logo.png">
+        <StickyNotes msg="Welcome to Your Vue.js App"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import StickyNotes from './components/StickyNotes'
+    import Header from './components/layout/Header'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            StickyNotes,
+            Header
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'poppin-reg', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
 </style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="relative bg-grey-lighter antialiased border-grey-lighter border-b-2 pt-24 pb-32 md:pb-40" id="banner">
+        <div class="max-w-lg mx-auto">
+            <h1 class="title text-center text-white text-white py-6 md:mx-24">Sticky Notes</h1>
+            <h2 class="text-2xl md:text-3xl text-center pt-8 text-grey-darkest font-normal leading-tight">A notes taking app <strong>that is shamelessly</strong> a clone of Google's <strong>Keep Notes</strong> app.</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Header'
+    }
+</script>
+
+<style scoped>
+    #banner {
+        background-color: #fff;
+        background-image: url('/img/bg.svg');
+    }
+
+    h1.title {
+        font-size: 3.5rem;
+        letter-spacing: 3px;
+        background-color: #f6993f;
+        background-image: url('/img/bgTitle.svg'); }
+</style>
